f260e49c54cc0f163d607be89333f5a08872e3e2
dezlymacauley <dezlymacauley@proton.me>
2026-08-12 14:39:28 +0200

    Created function to seed authors table

--- src/mock_database.rs
+++ src/mock_database.rs
@@ -1,4 +1,4 @@
 // Declares `schema.rs` and `seed_data_authors_table.rs` 
-// as part of the `route_handlers` module
+// as part of the `mock_database` module
 pub mod schema;
 pub mod seed_data_authors_table;
--- src/mock_database/seed_data_authors_table.rs
+++ src/mock_database/seed_data_authors_table.rs
@@ -1,21 +1,24 @@
 /*
-  ABOUT: src/mock-database/authors_table_seed_data.ts
+  ABOUT: src/mock-database/seed_data_authors_table.rs
 
   Initial seed data for the authors table of the mock-database
 */
 
-// Imports the AuthorsTable type
-// import type { AuthorsTable } from "./schema";
+// Imports the Author type and AuthorsTable type
+use super::schema::{Author, AuthorsTable};
 
-// Creates a new instance of the Authors table,
-// with starting data.
-// export const authorsTable: AuthorsTable = [
-//   {
-//     id: "5ed614ec-b3cb-4b37-9f19-4304e5574fd5",
-//     name: "Seth Baradock"
-//   },
-//   {
-//     id: "2221a287-b633-4473-950b-ba4e5b6e6632",
-//     name: "Cassie Elmore"
-//   }
-// ];
+// This is a function that will create a new instance of an `AuthorsTable`,
+// that already contains initial data.
+// The pub keyword allows this function to be used outside of this file
+pub fn seedAuthorsTable() -> AuthorsTable {
+    vec![
+        Author {
+            id: String::from("5ed614ec-b3cb-4b37-9f19-4304e5574fd5"),
+            name: String::from("Seth Baradock")
+        },
+        Author {
+            id: String::from("2221a287-b633-4473-950b-ba4e5b6e6632"),
+            name: String::from("Cassie Elmore")
+        }
+    ]
+}
